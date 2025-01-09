1ccda656bfde7fcbe0394805340a2c9acba7a0b9
morgancarre <morgancarre4@gmail.com>
2025-01-10 09:38:58 +1100

    ajout données des forfaits M #28

---
+++ src/main/resources/db/migration/V2__create_and_feed_forfait_m.sql
@@ -0,0 +1,14 @@
+-- Créer la table forfait_m
+CREATE TABLE forfait_m (
+    id VARCHAR(255) PRIMARY KEY,
+    volumetrie VARCHAR(255),
+    vocal VARCHAR(255),
+    sms VARCHAR(255),
+    prix DECIMAL(10, 2),
+    url VARCHAR(255) NOT NULL
+);
+
+-- Charger les données depuis forfait_m.csv
+INSERT INTO forfait_m (id, volumetrie, vocal, sms, prix, url)
+SELECT id, volumetrie, vocal, sms, prix, url
+FROM CSVREAD('classpath:db/data/forfait_m.csv');
