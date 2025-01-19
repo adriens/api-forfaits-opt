274d0e20a44b6ad5fdc726a8d27b7e06a3654115
morgancarre <morgancarre4@gmail.com>
2025-01-20 10:32:48 +1100

    ajout de description des paramètres #33

--- src/main/java/nc/opt/api/Forfait.java
+++ src/main/java/nc/opt/api/Forfait.java
@@ -1,5 +1,7 @@
 package nc.opt.api;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -8,9 +10,13 @@ import jakarta.persistence.Table;
 @Table(name = "forfaits") // map la table forfait avec l'entitée
 public class Forfait {
     @Id
+    @Schema(description = "Identifiant de la gamme du forfait.")
     private String id;
+    @Schema(description = "Nom de la gamme du forfait.")
     private String desc;
+    @Schema(description = "Description du forfait.")
     private String description;
+    @Schema(description = "URL de la page du forfait sur le site Officiel.")
     private String url;
     
     public String getId() {
--- src/main/java/nc/opt/api/ForfaitM.java
+++ src/main/java/nc/opt/api/ForfaitM.java
@@ -1,19 +1,28 @@
 package nc.opt.api;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "forfait_m") // map la table forfait_m avec l'entité
+@Schema(description = "Forfait de la gamme M")
 public class ForfaitM {
 
     @Id
+    @Schema(description = "Identifiant du forfait.")
     private String id;
+    @Schema(description = "Quantité de données mobiles.")
     private String volumetrie;
+    @Schema(description = "Temps d'appel disponible.")
     private String vocal;
+    @Schema(description = "Nombre de SMS disponibles.")
     private String sms;
+    @Schema(description = "Prix du forfait.")
     private Double prix;
+    @Schema(description = "URL de la page du forfait sur le site officiel.")
     private String url;
 
     public String getId() {
